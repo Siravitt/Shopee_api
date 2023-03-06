6886775cda911e994aa215ac00acca2b55f7b73c
Wannarak2659 <wannarak.wan@hotmail.com>
2023-03-06 18:39:38 +0700

    validate-update- profile

--- src/validators/profileUser-validate.js
+++ src/validators/profileUser-validate.js
@@ -2,7 +2,10 @@ const Joi = require("joi");
 const validate = require("./validate");
 
 const editUserProfileSchema = Joi.object({
+  firstName: Joi.string().trim(),
+  lastName: Joi.string().trim(),
   userName: Joi.string().trim(),
+  email: Joi.string().trim(),
   phone: Joi.string().pattern(/^[0-9]{10}$/),
 });
 
